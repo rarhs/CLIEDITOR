bd1f1e6dc5b167fb480b3530bed2f559e0e512a0
rarhs <reza054@gmail.com>
2023-04-03 14:01:11 +0600

    Create a function that open and read the contents of a file.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import sys
 import os
+
+
+def open_and_read_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            file_contents = file.readlines()
+        return file_contents
+    else:
+        return []
