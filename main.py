b42c4bc7249c2583418de2dad302cf0045eea43b
rarhs <reza054@gmail.com>
2023-04-03 14:38:58 +0600

    Create a function that write the contents of a file.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-
+# Function to open and read the contents of a file.
 def open_and_read_file(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
@@ -9,3 +9,11 @@ def open_and_read_file(file_path):
         return file_contents
     else:
         return []
+
+# Function to write the contents of a file.
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        for i, line in enumerate(content):
+            file.write(line)
+            if i != len(content) - 1:
+                file.write('\n')
